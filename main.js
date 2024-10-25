b1356ec55b4afc4f171e95c365571824aead19ff
changhong <hongfour2022_4@163.com>
2024-10-25 22:13:48 +0800

    Open a window if none are open (macOS)

--- main.js
+++ main.js
@@ -11,10 +11,13 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 app.on('window-all-closed', () => {
   console.log(process.platform);
-  
+
   if (process.platform !== 'darwin') app.quit();
 });
